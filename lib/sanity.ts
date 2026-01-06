65d7d57259eb30f41de9fb2cc8b59a89db726ab3
Mohammed-Burhani <jhansi@burhanitechnologies.com>
2026-01-06 14:32:29 +0530

    Removed unneccesary cred

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '6qm97z3a',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   useCdn: false, // Set to false to get fresh data without CDN caching
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01',
